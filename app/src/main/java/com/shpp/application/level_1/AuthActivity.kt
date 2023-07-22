ceaf13c1dc481daf6eebf23597bc85d4544c2467
Pavlo <pavlo.kokhanevych@nure.ua>
2023-07-22 15:03:20 +0300

    finished 1 task 1 level

---
+++ app/src/main/java/com/shpp/application/level_1/AuthActivity.kt
@@ -0,0 +1,12 @@
+package com.shpp.application.level_1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.shpp.application.R
+
+class AuthActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.auth_activity)
+    }
+}
